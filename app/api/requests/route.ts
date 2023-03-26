4baf39d2667338796be27879b78cd23a0beb93e2
Joe Klakus <joeyk2k@hotmail.com>
2023-03-26 19:48:38 +0800

    styled

--- app/api/requests/route.ts
+++ app/api/requests/route.ts
@@ -11,14 +11,20 @@ const openai = new OpenAIApi(configuration);
 
 export async function POST(request: Request) {
   const req = await request.json();
+
+  const prompt = `make me a readme file with the title ${req.projectName}, long description based on ${req.description} and explain why i used 
+    these npm packages ${req.npmpackages} when listing the packages in a list don't include any types and give a brief explaination of what they do.
+    include how to install the project using ${req.installation} command. How to run it using the ${req.usage} command. Also include 
+    the ${req.contributors} and the ${req.license} unless it is none don't include it`;
+
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: req,
+    prompt: prompt,
     max_tokens: 2048,
     temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   });
-  return NextResponse.json(completion.data.choices[0].text);
+  return NextResponse.json(completion.data.choices[0].text?.trim());
 }
